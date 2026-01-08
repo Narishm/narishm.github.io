3a190458c1a9c79586d9caabad662f14be504cc1
Narishm <narishm@gmail.com>
2026-01-08 01:11:57 -0500

    Deploy site

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// config.js
+window.GCAL_CONFIG = {
+  apiKey: "AIzaSyBoy744z7CH_M5IETsfw8VFZ4-LmCh_vFI",
+  calendarShareLink: "https://calendar.google.com/calendar/u/0?cid=bmFyaXNobUBnbWFpbC5jb20"
+};
